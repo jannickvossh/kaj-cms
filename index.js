8df24db89971bc14a25d0630559f4bdac9210109
minihauner <jannickhaunstrup@gmail.com>
2025-04-26 19:55:03 +0200

    opsæt index.html, og send den som respons på /

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 import express from "express";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
 const app = express();
 const port = 3000;
 
 app.get("/", (req, res) => {
-    res.send("Hello, world!");
+    res.sendFile(`${__dirname}/public/index.html`);
 });
 
 app.listen(port, () => {
