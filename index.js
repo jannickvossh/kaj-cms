f843332e6bb8261cac255f6df32ed46414e488ec
minihauner <jannickhaunstrup@gmail.com>
2025-05-12 01:16:34 +0200

    fejlfinding i config vars

--- index.js
+++ index.js
@@ -3,7 +3,9 @@ import bodyParser from 'body-parser';
 import fileUploadPkg from 'express-fileupload';
 const fileUpload = fileUploadPkg;
 import dotenv from 'dotenv';
-dotenv.config();
+if (process.env.NODE_ENV !== "production") {
+    dotenv.config();
+}
 import { marked } from 'marked';
 import { minify } from 'htmlfy';
 import nodeHtmlMarkdownPkg from 'node-html-markdown';
